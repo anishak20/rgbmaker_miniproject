fbba33cb95b7b5f4278287ad2b13cffd6fa6a57b
Avinash Kumar <avialxee@gmail.com>
2024-07-19 10:01:43 +0300

    cmin value

--- src/rgbc/cli.py
+++ src/rgbc/cli.py
@@ -16,12 +16,15 @@ parser.add_argument('-s', '--surveys', type=str, help="""comma separated survey 
                     Ex. rgbc -s 'DSS2 IR,DSS2 Red, DSS2 Blue' ...
                     """)
 
+parser.add_argument('-cmin', '--contour-min', type=str, help="""give the lower level value for plotting the contour
+                    """)
+
 def cli():
     args=parser.parse_args()
 
     position    =   args.position
     radius      =   args.radius
     surveys     =   args.surveys.split(',')
+    cmin        =   float(args.contour_min)
 
-    RGBC(position=position, radius=radius, svy=surveys, kind='png').plot()
-
+    RGBC(position=position, radius=radius, svy=surveys, cmin=cmin, kind='png').plot()
